364fcd6b472b0dcf6dd868b77d4a482f5a9586a2
Hamna abbas <112249808+hamnaabbas@users.noreply.github.com>
2025-10-17 16:35:39 +0500

    Update Event.js

--- controllers/Event.js
+++ controllers/Event.js
@@ -4,15 +4,24 @@ function isValidEvent(event) {
   }
 
   const eventDate = new Date(event.date);
-  const now = new Date();
-
-//   // BUG: Should not allow past dates, but currently allows them
   if (isNaN(eventDate.getTime())) {
     return { valid: false, message: "Invalid date format" };
   }
-   
+
+  // Get today's date at midnight (ignore time)
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  // Also set eventDate time to midnight to compare only dates
+  eventDate.setHours(0, 0, 0, 0);
+
+  // Reject past dates
+  if (eventDate < today) {
+    return { valid: false, message: "Event date cannot be in the past" };
+  }
 
   return { valid: true, message: "Event is valid" };
 }
 
 module.exports = { isValidEvent };
+
